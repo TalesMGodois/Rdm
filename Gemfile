ffa236d745db515a6cca0aed62065019319ed9cf
TalesMGodois <tales.godois@gmail.com>
2016-02-08 13:40:40 -0200

    Commit inicial do projeto

---
+++ Gemfile
@@ -0,0 +1,14 @@
+# A sample Gemfile
+source "https://rubygems.org"
+
+gem 'roo', '~> 2.1.0'
+
+gem 'googlecharts', '~> 1.6', '>= 1.6.12'
+
+gem 'mongo', '~> 2.2', '>= 2.2.2'
+
+gem 'gemcutter', '~> 0.7.1'
+
+gem 'mongo_ext', '~> 0.19.3'
+
+# gem "rails"
---
+++ Main.rb
@@ -0,0 +1,12 @@
+require 'roo'
+require 'googlecharts'
+
+selic = Roo::Spreadsheet.open("sheets/Selic.xlsx")
+
+selic.sheet('planilha').column(1)[1]
+
+puts Gchart.bar( :data => [[1,2,4,67,100,41,234],[45,23,67,12,67,300, 250]], 
+            :title => 'SD Ruby Fu level', 
+            :legend => ['matt','patrick'], 
+            :bar_colors => 'ff0000,00ff00')
+
