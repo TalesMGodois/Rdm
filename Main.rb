0c52881532a6ffe8989ffb7eab6b030003c405b4
Tales Marinho Godois <tales.godois@tre-go.jus.br>
2016-02-10 18:53:05 -0200

    alterano main e modulo DB

--- Main.rb
+++ Main.rb
@@ -2,12 +2,12 @@ require 'roo'
 require 'googlecharts'
 
 
-Plan = Roo::Spreadsheet.open("sheets/Selic.xlsx")
+Plan = Roo::Spreadsheet.open("lib/db/sheets/Selic.xlsx")
 
-Plan.sheet('planilha').column(1)[1]
+puts Plan.sheet('planilha').column('A')
 
-puts Gchart.bar( :data => [[1,2,4,67,100,41,234],[45,23,67,12,67,300, 250]], 
-            :title => 'SD Ruby Fu level', 
-            :legend => ['matt','patrick'], 
-            :bar_colors => 'ff0000,00ff00')
+# puts Gchart.bar( :data => [[1,2,4,67,100,41,234],[45,23,67,12,67,300, 250]], 
+#             :title => 'SD Ruby Fu level', 
+#             :legend => ['matt','patrick'], 
+#             :bar_colors => 'ff0000,00ff00')
 
--- lib/db/db_connect.rb
+++ lib/db/db_connect.rb
@@ -2,12 +2,53 @@ require 'roo'
 
 module DB
 
-	table = Roo::Spreadsheet.open("sheets/dados_economicos.xlsx")
-
-	SELIC = table.sheet('Selic_Otimizado')
-	PIB = table.sheet('Pib_Otimizado')
-	SELIC = table.sheet('IPCA_Otimizado')
-	SELIC = table.sheet('Dolar_Otimizado')
 
 
+	SELIC	= "S"
+	PIB 	= "P"
+	IPCA 	= "I"
+	DOLAR  	= "D"
+
+	TABLE = Hash.new()
+
+	
+	pib_ot = Hash.new()
+	ipca_ot = Hash.new()
+	dolar_ot = Hash.new()
+
+	# table 		= Roo::Spreadsheet.open("../sheets/Dados_Tabelados.xlsx")
+	table = Roo::Spreadsheet.open("lib/db/sheets/Dados_Tabelados.xlsx")
+	
+	TABLE[:selic] = table.sheet('Selic_Otimizado')
+	TABLE[:pib] 	= table.sheet('Pib_Otimizado')
+	TABLE[:ipca] 	= table.sheet('IPCA_Otimizado')
+	TABLE[:dolar] = table.sheet('Dolar_Otimizado')
+
+	def self.simplify
+		selic_ot = Hash.new()
+		aux = 2015 - 2007
+		aux = aux + 1
+		months = TABLE[:selic].column(1)
+		for i in 1 .. aux
+			data = TABLE[:selic].column(i + 1)
+			year = data.shift()
+
+			data.each do |item|
+
+				selic_ot["#{months[i]}-#{year}"] = item
+			end
+		end
+
+		puts selic_ot
+
+	end
+
+	def modify(ant,prox)
+		return 1 if([ant,prox].max == prox) 
+		return 0 if([ant,prox].max == ant) 
+	end
+	
 end
+
+
+DB::simplify()
